ecf296814bbfd2fd7930820126c063062ca04257
liushen <lius484441475@gmail.com>
2023-02-11 18:02:56 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+    private JFrame frame=new JFrame("Taheo-Lafs");
+    private GridBagLayout gbl=new GridBagLayout();
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private Panel leftPanel=new Panel();
+    private JButton leftButton=new JButton();
+
+    void Init()
+    {
+        frame.setLayout(gbl);
+        frame.setVisible(true);
+        frame.setSize(1100,650);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        leftPanel.setLayout(gbl);
+        leftPanel.setSize(70,650);
+
+        leftButton.setSize(70,70);
 
 
+
+    }
 
     public static void main(String[] args)
     {
-
+        new Main().Init();
     }
 }
