f297812b2197f379e6973b451a53a0ff0a00f002
liushen <lius484441475@gmail.com>
2023-02-11 17:46:23 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,9 @@ import javax.swing.*;
 public class Main {
 
 
+
     public static void main(String[] args)
     {
-        JFrame frame =new JFrame("Tahoe-Lafs");
-
-
-        frame.setVisible(true);
 
     }
 }
