0200e002227f4ab4681656a7acff3d0aaf3b8366
liushen <lius484441475@gmail.com>
2023-02-11 18:15:43 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,26 @@ import java.awt.*;
 
 public class Main {
     private JFrame frame=new JFrame("Taheo-Lafs");
-    private GridBagLayout gbl=new GridBagLayout();
-    private GridBagConstraints gbc = new GridBagConstraints();
+    private GridBagLayout gridBagLayout=new GridBagLayout();
+    private GridBagConstraints gridBagConstraints = new GridBagConstraints();
     private Panel leftPanel=new Panel();
     private JButton leftButton=new JButton();
 
     void Init()
     {
-        frame.setLayout(gbl);
-        frame.setVisible(true);
+        frame.setLayout(gridBagLayout);
         frame.setSize(1100,650);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.setBackground(Color.CYAN);
+        frame.setVisible(true);
 
-        leftPanel.setLayout(gbl);
+        gridBagConstraints.fill=gridBagConstraints.BOTH;//该方法是为了设置如果组件所在的区域比组件本身要大时的显示情况
+
+
+
+
+        leftPanel.setLayout(gridBagLayout);
         leftPanel.setSize(70,650);
 
         leftButton.setSize(70,70);
