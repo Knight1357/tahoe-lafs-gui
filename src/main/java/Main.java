3fceca4435d325e6a9b1ee160f2c92376217efc6
liushen <lius484441475@gmail.com>
2023-02-11 19:24:44 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,29 +6,65 @@ public class Main {
     private GridBagLayout gridBagLayout=new GridBagLayout();
     private GridBagConstraints gridBagConstraints = new GridBagConstraints();
     private Panel leftPanel=new Panel();
-    private JButton leftButton=new JButton();
+
+    private JButton homePageButton=new JButton();
+    private JButton transmissionButton=new JButton();
+    private JButton setButton=new JButton();
+
+    private Panel middlePanel=new Panel();
 
     void Init()
     {
-        frame.setLayout(gridBagLayout);
+        gridBagConstraints.fill=gridBagConstraints.BOTH;//该方法是为了设置如果组件所在的区域比组件本身要大时的显示情况
+//        gridBagLayout.setConstraints(leftPanel,gridBagConstraints);
+
+//        frame.setLayout(gridBagLayout);
+        frame.setLayout(new FlowLayout());
         frame.setSize(1100,650);
+        frame.setMinimumSize(new Dimension(1100,650));
+//        frame.setMaximumSize(new Dimension(1100,650));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        frame.setBackground(Color.CYAN);
         frame.setVisible(true);
 
-        gridBagConstraints.fill=gridBagConstraints.BOTH;//该方法是为了设置如果组件所在的区域比组件本身要大时的显示情况
 
 
-
-
-        leftPanel.setLayout(gridBagLayout);
+        leftPanel.setLayout(new GridBagLayout());
         leftPanel.setSize(70,650);
+        leftPanel.setBackground(Color.CYAN);
 
-        leftButton.setSize(70,70);
+        homePageButton.setText("首页");
+        homePageButton.setSize(70,70);
+        homePageButton.setBackground(Color.GRAY);
+        homePageButton.setLocation(0,0);
+
+
+        transmissionButton.setText("传输");
+        transmissionButton.setSize(70,70);
+        transmissionButton.setBackground(Color.GRAY);
+        transmissionButton.setLocation(0,70);
+
+        setButton.setText("设置");
+        setButton.setSize(70,70);
+        setButton.setBackground(Color.GRAY);
+//        setButton.setLocation(0,650-110);
+
+        middlePanel.setLayout(new CardLayout());
+        middlePanel.setLocation(70,0);
+        middlePanel.setSize(170,650);;
+        middlePanel.setBackground(Color.PINK);
 
 
 
+
+
+
+        leftPanel.add(homePageButton);
+        leftPanel.add(transmissionButton);
+        leftPanel.add(setButton);
+
+        frame.add(leftPanel);
+        frame.add(middlePanel);
     }
 
     public static void main(String[] args)
