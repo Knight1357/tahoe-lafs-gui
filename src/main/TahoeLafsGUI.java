8c9b65ba4d0a544c1520873e03ace9ff85aaa445
liushen <lius484441475@gmail.com>
2023-02-22 10:30:50 +0800

    update

--- src/main/TahoeLafsGUI.java
+++ src/main/TahoeLafsGUI.java
@@ -3,70 +3,15 @@ import java.io.File;
 
 public class TahoeLafsGUI {
     private JPanel MainWindows;
-    private JPanel Left;
-    private JButton Myfile;
-    private JPanel Right;
-    private JPanel FileType;
-    private JButton Recycle;
-    private JPanel Set;
-    private JButton Seting;
-    private JPanel Function;
-    private JPanel FileManagement;
-    private JButton Upload;
-    private JButton NewFile;
-    private JScrollPane FileListScroll;
-
-    public DefaultListModel model=new DefaultListModel();
-    public JList fileList=new JList();
-
-
+    private JPanel UpPanel;
+    private JScrollPane DownScrollPanel;
 
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Tahoe-Lafs");
+        JFrame frame = new JFrame("TahoeLafsGUI");
         frame.setContentPane(new TahoeLafsGUI().MainWindows);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setLocationRelativeTo(null);
-
         frame.setVisible(true);
-
-        TahoeLafsGUI tahoeLafsGUI=new TahoeLafsGUI();
-//        tahoeLafsGUI.loadDir(new File("C:/"));
-        tahoeLafsGUI.Init("data");
-
-
     }
-
-    void Init(String path)
-    {
-
-        model.addElement("1");
-        model.addElement("2");
-        model.addElement("3");
-        model.addElement("4");
-
-        fileList.setModel(model);
-        fileList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        FileListScroll.setViewportView(fileList);
-
-        FileListScroll.setVisible(true);
-        fileList.setVisible(true);
-
-
-
-    }
-
-//    void loadDir(File dir)
-//    {
-//        File[] files =dir.listFiles();
-//        if(files==null)
-//            return;
-//        for(File f:files)
-//        {
-//            FileNode temp=new FileNode(f);
-//            model.addElement(temp);
-//        }
-//    }
-
 }
