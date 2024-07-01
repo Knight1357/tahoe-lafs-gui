2c8e44faa8e10f37bfac3103f6ca90f4580f8935
Knight1357 <lius484441475@gmail.com>
2024-07-01 21:36:35 +0800

    move

---
+++ src/main/java/com/tahoelafsgui/pojo/dto/FileList.java
@@ -0,0 +1,15 @@
+package com.tahoelafsgui.pojo.dto;
+
+import javax.swing.*;
+
+
+/**
+ * @author liushen
+ */
+public class FileList<T> extends JList<T> {
+    String filename;
+
+    public FileList() {
+    }
+}
+
