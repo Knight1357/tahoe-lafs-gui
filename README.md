59459f33fed566b270ef4b0bd89b4b187cd3ba24
Knight1357 <lius484441475@gmail.com>
2024-07-01 20:39:33 +0800

    update

---
+++ README.md
@@ -0,0 +1,34 @@
+# tahoe-lafs-gui
+
+tahoe-lafs-gui 是一个用于 tahoe-lafs 分布式存储系统的 GUI 客户端。它提供了一个简单直观的界面，通过发送和接收 HTTP 请求来执行分布式存储系统的文件操作。
+
+## 功能特性
+
+- 通过 GUI 客户端上传、下载、删除文件等文件操作。
+- 支持与 tahoe-lafs 分布式存储系统的通信，通过 HTTP 请求与服务器进行交互。
+- 使用 Java Swing 编写的用户界面，易于使用和定制。
+
+## 安装说明
+
+1. 克隆本仓库到本地计算机：
+```shell
+git clone https://github.com/Knight1357/tahoe-lafs-gui.git
+```
+2. 进入项目目录：
+```shell
+cd tahoe-lafs-gui
+```
+3. 编译运行项目:
+```shell
+javac TahoeLafs
+java TahoeLafs
+```
+
+## 使用指南
+1. 启动 GUI 前, 你应先运行 tahoe-lafs 系统
+2. 在 Constant 中, 修改运行 tahoe-lafs 主机的ip, 以及新建的根目录文件夹
+3. 编译运行 TahoeLafs
+
+## 注意事项
+
+这是我写的第一个小项目, 代码中有许多不规范的地方
--- src/config/Constant.java
+++ src/config/Constant.java
@@ -8,7 +8,6 @@ public class Constant {
     //用户根目录路径
     private static final String USER_PATH = "URI:DIR2:wti64bj6ovkrubpngomrldezpi:i7xev2c7bgrsu4remvipyeqzsb2qbrcbgteywvcijx5qetbc4pka";
 
-
     private static String introducerAPI = "127.0.0.1:3456";
 
     private static ArrayList<String> filelocationlist = new ArrayList<>() {
@@ -34,9 +33,12 @@ public class Constant {
     }
 
 
-    public static int nowfile = 0;//文件指向
-    private static FileNode parentNode;//父级文件夹路径
-    private static FileNode isSelectFileNode;//目前选中的文件
+    //文件指向
+    public static int nowfile = 0;
+    //父级文件夹路径
+    private static FileNode parentNode;
+    //目前选中的文件
+    private static FileNode isSelectFileNode;
 
 
     public static FileNode getParentNode() {
