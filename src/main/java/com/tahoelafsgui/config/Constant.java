506b99de0f2e28e7f5f7ef93c6ef9a2441c7e959
Knight1357 <lius484441475@gmail.com>
2024-07-01 21:35:37 +0800

    update comment

--- src/main/java/com/tahoelafsgui/config/Constant.java
+++ src/main/java/com/tahoelafsgui/config/Constant.java
@@ -1,21 +1,24 @@
 package com.tahoelafsgui.config;
 
+import com.tahoelafsgui.pojo.FileNode;
 import java.util.ArrayList;
 
 /**
  * @author liushen
- */ // 配置文件
+ */
+// 配置文件
 public class Constant {
-    //用户根目录路径
+    // 用户根目录路径
     private static final String USER_PATH = "URI:DIR2:wti64bj6ovkrubpngomrldezpi:i7xev2c7bgrsu4remvipyeqzsb2qbrcbgteywvcijx5qetbc4pka";
 
     private static String introducerAPI = "127.0.0.1:3456";
 
+    // 控制前进退回
     private static final ArrayList<String> filelocationlist = new ArrayList<>() {
         {
-            add(USER_PATH);//初始化链表
+            add(USER_PATH);// 初始化链表
         }
-    };//控制前进退回
+    };
 
     public static String getIntroducerAPI() {
         return introducerAPI;
@@ -34,11 +37,11 @@ public class Constant {
     }
 
 
-    //文件指向
+    // 文件指向
     public static int currentFile = 0;
-    //父级文件夹路径
+    // 父级文件夹路径
     private static FileNode parentNode;
-    //目前选中的文件
+    // 目前选中的文件
     private static FileNode isSelectFileNode;
 
 
@@ -50,16 +53,17 @@ public class Constant {
         Constant.parentNode = parentNode;
     }
 
-    public static String getUserPath() {//获得根目录路径
+    // 获得根目录路径
+    public static String getUserPath() {
         return USER_PATH;
     }
 
-
-    public static FileNode getIsSelectFileNode() {//获得目前选中的文件
+    // 获得目前选中的文件
+    public static FileNode getIsSelectFileNode() {
         return isSelectFileNode;
     }
 
-    //设置当前选中的文件
+    // 设置当前选中的文件
     public static void setIsSelectFileNode(FileNode isSelectFileNode) {
         Constant.isSelectFileNode = isSelectFileNode;
         System.out.println("设置了 " + isSelectFileNode.getName());
