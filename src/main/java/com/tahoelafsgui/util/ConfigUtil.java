9195543a1e005dcf8c91548e34b0c5619325f4eb
Knight1357 <lius484441475@gmail.com>
2024-07-02 17:39:47 +0800

    add ConfigUtil.java

---
+++ src/main/java/com/tahoelafsgui/util/ConfigUtil.java
@@ -0,0 +1,28 @@
+package com.tahoelafsgui.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author liushen
+ */
+public class ConfigUtil {
+    private static Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigUtil.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
