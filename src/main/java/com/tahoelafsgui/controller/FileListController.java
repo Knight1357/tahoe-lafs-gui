c80b41673c1205705e35a7d3f8cca7d11ad129c5
Knight1357 <lius484441475@gmail.com>
2024-07-02 20:45:29 +0800

    add FileListController.java

---
+++ src/main/java/com/tahoelafsgui/controller/FileListController.java
@@ -0,0 +1,45 @@
+package com.tahoelafsgui.controller;
+
+import com.tahoelafsgui.config.Constant;
+import com.tahoelafsgui.gui.panel.FileScrollPanel;
+import com.tahoelafsgui.pojo.FileNode;
+import com.tahoelafsgui.pojo.FileStructure;
+import com.tahoelafsgui.util.JsonUtil;
+
+/**
+ * @author liushen
+ */
+// 文件列表操作
+public class FileListController {
+
+
+    // 读取文件夹文件
+    public static void loadDir(String hashVal) {
+        JsonUtil.toJson();
+        JsonUtil.fromJson();
+        if (FileStructure.getFileStructure().containsKey(hashVal)) {
+            FileNode temp = FileStructure.getFileStructure().get(hashVal);
+            if (!hashVal.isEmpty()) {
+                // 清空所有元素
+                FileScrollPanel.getFileListModel().removeAllElements();
+            }
+            // 文件夹，插入文件夹中的元素
+            if (temp.isDir()) {
+                // 记录父级文件夹
+                Constant.setParentNode(temp);
+                for (String i : temp.getFileList()) {
+                    FileNode cur = FileStructure.getFileStructure().get(i);
+                    FileScrollPanel.getFileListModel().addElement(cur);
+                }
+            } else {
+                // 文件，插入当前文件
+                FileScrollPanel.getFileListModel().addElement(temp);
+            }
+            System.out.println("成功读取文件");
+        } else {
+            // 清空所有元素
+            FileScrollPanel.getFileListModel().removeAllElements();
+            System.out.println("查无此文件");
+        }
+    }
+}
