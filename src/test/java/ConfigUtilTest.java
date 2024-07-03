2f0280ef9bbf2e136e2ad77eda790ca5514d8544
Knight1357 <lius484441475@gmail.com>
2024-07-03 13:49:14 +0800

    add ConfigUtilTest.java

---
+++ src/test/java/ConfigUtilTest.java
@@ -0,0 +1,18 @@
+
+import com.tahoelafsgui.util.ConfigUtil;
+
+public class ConfigUtilTest {
+    public static void main(String[] args) {
+        // 读取配置属性示例
+        ConfigUtil configManager = ConfigUtil.getInstance();
+        String directoryPath = configManager.getProperty("introducer.ip");
+        System.out.println("Introducer ip from config: " + directoryPath);
+
+        String dataPath = configManager.getProperty("data.path");
+        System.out.println("Data path from config: " + dataPath);
+
+        String picsPath = configManager.getProperty("pics.path");
+        System.out.println("Pictures path from config: " + picsPath);
+
+    }
+}
