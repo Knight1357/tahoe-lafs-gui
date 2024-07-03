b4eb739fd79d6fe3493b79a48eb83db0e9190387
Knight1357 <lius484441475@gmail.com>
2024-07-03 15:55:05 +0800

    update singleton pattern

--- src/main/java/com/tahoelafsgui/pojo/FileStructure.java
+++ src/main/java/com/tahoelafsgui/pojo/FileStructure.java
@@ -13,19 +13,38 @@ import java.util.HashMap;
 @Data
 public class FileStructure {
     // 哈希表文件信息存储
-    @Getter
     @Setter
-    public static HashMap<String, FileNode> fileStructure;
+    private static HashMap<String, FileNode> fileStructure;
 
-    public FileStructure(HashMap<String, FileNode> hashMap) {
-        fileStructure = hashMap;
-    }
+    // 单例实例
+    private static FileStructure instance;
 
-    public FileStructure() {
+    // 私有构造函数，防止外部实例化
+    private FileStructure() {
         fileStructure = new HashMap<>();
     }
 
-    public static void addFileNode(String fileName, String newUrl) {
-        fileStructure.get(fileName).addFile(newUrl);
+    // 提供一个全局访问点获取单例实例
+    public static synchronized FileStructure getInstance() {
+        if (instance == null) {
+            instance = new FileStructure();
+        }
+        return instance;
+    }
+
+    public HashMap<String, FileNode> getFileStructure() {
+        return fileStructure;
+    }
+
+    public void addFileNode(String fileName, String newUrl) {
+        if (fileStructure.containsKey(fileName)) {
+            fileStructure.get(fileName).addFile(newUrl);
+        } else {
+            // 如果不存在则新建一个 FileNode 对象并添加
+            FileNode fileNode = new FileNode(fileName, "");
+            fileNode.addFile(newUrl);
+            fileStructure.put(fileName, fileNode);
+        }
     }
 }
+
